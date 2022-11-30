8cf55ce9e194f51088c51464aa1891b8712dc9a0
Neotastisch <54811660+Neotastisch@users.noreply.github.com>
2022-11-30 17:54:13 +0100

    Update free.lua

--- free.lua
+++ free.lua
@@ -34,12 +34,7 @@ setfpscap(fps)
 end
 
 
-local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-wait(1)
-vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
+
 
 local function onChatted(p,msg)
     if player.name==admin then
