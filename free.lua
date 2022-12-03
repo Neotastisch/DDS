a0a9be268f425d8026941a41d62ef821cd32bd3d
Neotastisch <54811660+Neotastisch@users.noreply.github.com>
2022-12-03 14:55:34 +0100

    Update free.lua

--- free.lua
+++ free.lua
@@ -97,6 +97,11 @@ local function onChatted(p,msg)
 end   
 end
 
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+        vu:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+        wait(1)
+        vu:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    end)
 
 onMessageDoneFiltering.OnClientEvent:Connect(function(messageData)
 local speaker, message = players[messageData.FromSpeaker], messageData.Message
