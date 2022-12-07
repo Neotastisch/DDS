0a3199046bdf7a86ccda08217678521b04ee27bd
Neotastisch <54811660+Neotastisch@users.noreply.github.com>
2022-12-07 20:02:52 +0100

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -15,7 +15,13 @@ local webhook = getgenv().webhook
 
 
 local adminpositions = {{-870,-38,-550},{-870,-38,-570},{-870,-38,-590},{-870,-38,-610},{-900,-38,-550},{-900,-38,-570},{-900,-38,-590},{-900,-38,-610}}
-
+local BringLocations = {
+	["bank"] = CFrame.new(-396.988922, 21.7570763, -293.929779, -0.102468058, -1.9584887e-09, -0.994736314, 7.23731564e-09, 1, -2.71436984e-09, 0.994736314, -7.47735651e-09, -0.102468058),
+	["admin"] = CFrame.new(-872.453674, -32.6421318, -532.476379, 0.999682248, -1.36019978e-08, 0.0252073351, 1.33811247e-08, 1, 8.93094043e-09, -0.0252073351, -8.59080007e-09, 0.999682248),
+	["klub"] = CFrame.new(-264.434479, 0.0355005264, -430.854736, -0.999828756, 9.58909574e-09, -0.0185054261, 9.92017934e-09, 1, -1.77993904e-08, 0.0185054261, -1.79799198e-08, -0.999828756),	
+	["vault"] = CFrame.new(-495.485901, 23.1428547, -284.661713, -0.0313318223, -4.10440322e-08, 0.999509037, 2.18453966e-08, 1, 4.17489829e-08, -0.999509037, 2.31427428e-08, -0.0313318223),
+	["train"] = CFrame.new(591.396118, 34.5070686, -146.159561, 0.0698467195, -4.91725913e-08, -0.997557759, 5.03374231e-08, 1, -4.57684664e-08, 0.997557759, -4.70177071e-08, 0.0698467195),	
+}
 if not game:IsLoaded() then
     game.Loaded:Wait();
 end
@@ -82,6 +88,19 @@ function SendMessage(Webhook, Message, Botname)
    local Data = game:GetService("HttpService"):PostAsync(API, Body, false, "application/json")
    return Data or nil;
 end
+
+local function GetPlayerFromString(str,ignore)
+	for i,Targ in pairs(game.Players:GetPlayers()) do 
+		if not ignore and Targ == Players then
+			continue
+		end
+		if Targ.Name:lower():sub(1,#str) == str:lower() or  Targ.DisplayName:lower():sub(1,#str) == str:lower()  then
+			return Targ
+		end
+	end
+	return nil
+end
+
 coroutine.wrap(function()
 while true do
 if bringname != "" then
@@ -303,10 +322,10 @@ local function onChatted(p,msg)
          if Args[1] == prefix.."bring" then
             if player.name == bringer then
 	    game.Players.LocalPlayer.Character.Head.Anchored = false
-            local targetHumanoid = Players:FindFirstChild(string.split(msg," ")[2])
-	    if Args[3] == "admin" then	
-            BringPlr(targetHumanoid,CFrame.new(adminpositions[0][1]))
-	    	else
+            local targetHumanoid = GetPlayerFromString(Args[2])
+	    if Args[3] then
+	    BringPlr(targetHumanoid,BringLocations[Args[3])		
+	    else
             BringPlr(targetHumanoid,nil)
 	    end
 	    end
