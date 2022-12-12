3acd7bf1357b49c2c3f4babbe58d3938773e0c51
Neotastisch <54811660+Neotastisch@users.noreply.github.com>
2022-12-12 10:13:29 +0100

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -163,93 +163,6 @@ wait()
 end
 end)()
 
-local function Setup(Type,Debugmode)
-	CmdSettings["Aura"] = nil
-	if Debugmode then
-		for PointInTable = 1,40 do
-			local Table = SetupsTable[Type]
-
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Table["Origin"]
-
-			local XAxis 
-			local ZAxis
-			local i 
-			if PointInTable <= 10 then
-				i = 1
-			elseif PointInTable <= 20 then
-				i = 2
-			elseif PointInTable <= 30 then
-				i = 3
-			elseif PointInTable <= 40 then
-				i = 4
-			end
-			if i == 1 then
-				if PointInTable <= Table["PerRow"]   then
-					XAxis = 0
-					if PointInTable == 1 then
-						ZAxis = 0
-					else
-						ZAxis = (PointInTable-1)*Table["ZMultiplier"]
-					end
-
-				end
-			else
-				local index = i*Table["PerRow"]
-				if (Table["PerRow"]*i) >= PointInTable then
-					XAxis = (i-1)*Table["XMultiplier"]
-					ZAxis = (i*Table["PerRow"]-PointInTable)*Table["ZMultiplier"]
-
-				end
-			end
-
-
-			game.Players.LocalPlayer.Character.Archivable = true
-			local clone = game.Players.LocalPlayer.Character:Clone()
-			clone.Parent = workspace
-			clone.HumanoidRootPart.CFrame = Table["Origin"]*CFrame.new(XAxis,0,ZAxis)
-		end
-	else
-		local Table = SetupsTable[Type]
-
-		local PointInTable = getgenv().PointInTable
-		local XAxis 
-		local ZAxis
-		local i
-		if PointInTable <= 10 then
-			i = 1
-		elseif PointInTable <= 20 then
-			i = 2
-		elseif PointInTable <= 30 then
-			i = 3
-		elseif PointInTable <= 40 then
-			i = 4
-		end
-
-		if i == 1 then
-			if PointInTable <= Table["PerRow"]   then
-				XAxis = 0
-				if PointInTable == 1 then
-					ZAxis = 0
-				else
-					ZAxis = (PointInTable-1)*Table["ZMultiplier"]
-				end
-
-			end
-		else
-			local index = i*Table["PerRow"]
-			if (Table["PerRow"]*i) >= PointInTable then
-				XAxis = (i-1)*Table["XMultiplier"]
-				ZAxis = (i*Table["PerRow"]-PointInTable)*Table["ZMultiplier"]
-
-			end
-		end
-
-
-		Players.Character.HumanoidRootPart.CFrame = Table["Origin"]*CFrame.new(XAxis,0,ZAxis)
-	end
-
-end
-
 
 function BringPlr(Target,POS)
 	if Target.Character and Target.Character:FindFirstChild("Humanoid") then
@@ -477,7 +390,12 @@ local function onChatted(p,msg)
 	    end
         end
         if msg:match(prefix.."setup") then
-            Setup("Admin")
+            for i, v in ipairs(alts) do
+                if v == player.name then
+                    player.Character.HumanoidRootPart.CFrame = CFrame.new(adminpositions[i][1],adminpositions[i][2],adminpositions[i][3])
+                    player.Character.HumanoidRootPart.Rotation = Vector3.new(0,0,0)
+                end
+            end
         end
 
 end   
