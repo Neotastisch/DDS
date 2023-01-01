bb5e9e240f579d7c8de286359f7d387f1747cf62
Neotastisch <54811660+Neotastisch@users.noreply.github.com>
2023-01-01 13:25:01 +0100

    Update free.lua

--- free.lua
+++ free.lua
@@ -8,7 +8,13 @@ local alts = getgenv().alts
 
 local adminpositions = {{-870,-38,-550},{-870,-38,-570},{-870,-38,-590},{-870,-38,-610},{-900,-38,-550},{-900,-38,-570},{-900,-38,-590},{-900,-38,-610},{-840,-38,-550},{-840,-38,-570},{-840,-38,-590},{-840,-38,-610}}
 
-
+local BringLocations = {
+	["bank"] = CFrame.new(-396.988922, 21.7570763, -293.929779, -0.102468058, -1.9584887e-09, -0.994736314, 7.23731564e-09, 1, -2.71436984e-09, 0.994736314, -7.47735651e-09, -0.102468058),
+	["admin"] = CFrame.new(-872.453674, -32.6421318, -532.476379, 0.999682248, -1.36019978e-08, 0.0252073351, 1.33811247e-08, 1, 8.93094043e-09, -0.0252073351, -8.59080007e-09, 0.999682248),
+	["club"] = CFrame.new(-264.434479, 0.0355005264, -430.854736, -0.999828756, 9.58909574e-09, -0.0185054261, 9.92017934e-09, 1, -1.77993904e-08, 0.0185054261, -1.79799198e-08, -0.999828756),	
+	["vault"] = CFrame.new(-495.485901, 23.1428547, -284.661713, -0.0313318223, -4.10440322e-08, 0.999509037, 2.18453966e-08, 1, 4.17489829e-08, -0.999509037, 2.31427428e-08, -0.0313318223),
+	["train"] = CFrame.new(591.396118, 34.5070686, -146.159561, 0.0698467195, -4.91725913e-08, -0.997557759, 5.03374231e-08, 1, -4.57684664e-08, 0.997557759, -4.70177071e-08, 0.0698467195),	
+}
 
 local dropping = false
 local Players = game:GetService("Players")
@@ -39,7 +45,13 @@ end)
 local function onChatted(p,msg)
     if player.name==admin then
          if msg:match(prefix.."tpto") then
-            local targetPlayer = Players:FindFirstChild(string.split(msg," ")[2])
+	    local targetPlayer = GetPlayerFromString(string.split(msg," ")[2])
+	    if targetPlayer then
+	    Players.LocalPlayer.Character.HumanoidRootPart.CFrame = targetPlayer.Character.HumanoidRootPart.CFrame
+	    else
+	    Players.LocalPlayer.Character.HumanoidRootPart.CFrame = BringLocations[Args[2]]	
+	    end
+            
             Players.LocalPlayer.Character.HumanoidRootPart.CFrame = targetPlayer.Character.HumanoidRootPart.CFrame
         end
      return
